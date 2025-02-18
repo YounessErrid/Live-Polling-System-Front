de588c60c5e67f7e6a4e304c312686552aa4d549
Ali AB <aliabdev07@gmail.com>
2025-02-18 17:49:32 +0100

    Update App.jsx
    - Integrate Protected route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './views/login'
+import { Route, Routes } from 'react-router'
+import Singup from './views/Singup'
+import { Home } from './views/Home'
+import ProtectedRoute from '../routes/ProtectedRoute'
 
 function App() {
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline text-red-600">
-    Hello world!
-  </h1>
-    </>
+    <Routes>
+
+      <Route element={<ProtectedRoute/>}>
+        <Route path='/' element={<Home/>}></Route>
+      </Route>
+
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/Singup" element={<Singup/>}/> 
+
+    </Routes>
   )
 }
 
