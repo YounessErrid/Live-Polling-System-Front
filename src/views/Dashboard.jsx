b96375f727e1287aa6083fe74aaa5a9140e813db
Ali AB <aliabdev07@gmail.com>
2025-02-19 20:22:12 +0100

    Create Dashboard.jsx

---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Poll from '../components/poll';
+import Navbar from '../components/Navbar';
+
+export const Dashboard = () => {
+  return (
+    <>
+      <div>Dashboard</div>
+      <p>Welcome to Dashboard</p>
+      <Poll/>
+    </>
+  )
+}
+export default Dashboard;
