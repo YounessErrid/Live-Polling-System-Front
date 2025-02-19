4d5144b7da2ecde5d610e4f77b64fc530d484b12
Ali AB <aliabdev07@gmail.com>
2025-02-19 20:22:03 +0100

    Update Home.jsx
    - Add Home page code

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import { useState } from 'react'
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Navbar from '../components/Navbar'
+import { Link } from 'react-router-dom'
 
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
+  
 export const Home = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
     <>
-        <div>Home</div>
-        <p>Welcome to home page</p>
+    <div className="bg-white">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-30">
+          <div className="text-center">
+            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+            Live Polling System, Vote in Real Time!
+            </h1>
+            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+                Create and participate in live polls with instant results! Engage your audience with real-time voting, see percentage breakdowns, and track vote counts dynamically. Perfect for events, meetings, and social interactions. Start polling now! 🚀
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <Link to="/Login" className="text-sm/6 font-semibold text-gray-900">
+                Login <span aria-hidden="true">→</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
+export default Home;
