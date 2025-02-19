93e782e9df4e483e3826a6655e414253051d5a88
Ali AB <aliabdev07@gmail.com>
2025-02-19 20:21:30 +0100

    Update Singup.jsx

--- src/views/Singup.jsx
+++ src/views/Singup.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export const Singup = () => {
   return (
     <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
             Create your account
