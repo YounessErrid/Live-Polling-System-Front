18fa39b6f64a813152591b1c59d84a26a7bb8c97
Ali AB <aliabdev07@gmail.com>
2025-02-20 13:05:40 +0100

    Update Dashboard.jsx

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -5,8 +5,11 @@ import Navbar from '../components/Navbar';
 export const Dashboard = () => {
   return (
     <>
-      <div>Dashboard</div>
-      <p>Welcome to Dashboard</p>
+      <header className="bg-white shadow-sm">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+        </div>
+      </header>
       <Poll/>
     </>
   )
