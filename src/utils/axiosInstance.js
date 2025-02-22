8c3e78bb02b752e4e3b6fbd0c569092f0c1f59bf
Youness <85195141+YounessErrid@users.noreply.github.com>
2025-02-23 00:50:21 +0100

    Update axiosInstance.js
    create axios  api and interceptor

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,9 +1,24 @@
 import axios from 'axios';
+import { ACCESS_TOKEN } from './constants';
 
-const instance = axios.create({
-  baseURL: 'http://localhost:3000/api', // Define your base API URL here
-  timeout: 10000, // Set a timeout for requests (optional)
-  withCredentials: true, // Include credentials with requests
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL, // Define your base API URL here
+  // timeout: 10000, // Set a timeout for requests (optional)
+  // withCredentials: true, // Include credentials with requests
 });
 
-export default instance;
+// Add a request interceptor
+
+api.interceptors.request.use((config) => {
+  // Add token to headers if it exists in local storage
+  const token = localStorage.getItem(ACCESS_TOKEN);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+},
+(error) => {
+  return Promise.reject(error);
+});
+
+export default api;
