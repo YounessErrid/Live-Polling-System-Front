b548d2aeac034fa979c8ec8b4a227eab1a478ae1
Youness <85195141+YounessErrid@users.noreply.github.com>
2025-02-23 00:50:57 +0100

    add login and logout functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './views/login'
-import { Route, Routes } from 'react-router'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Singup from './views/Singup'
 import Dashboard from './views/Dashboard'
 import ProtectedRoute from '../routes/ProtectedRoute'
 import Home from './views/Home'
 import Navbar from './components/Navbar'
+import Logout from './views/Logout'
+import NotFound from './views/NotFound'
+// function Logout() {
+//   localStorage.removeItem('access_token');
+//   localStorage.removeItem('refresh_token');
+//   return <Navigate to="/Login" />;
+// }
+function RegisterAndLogout() {
+  localStorage.removeItem('access_token');
+  localStorage.removeItem('refresh_token');
+  return <Navigate to="/Login" />;
+}
 
 function App() {
 
@@ -17,11 +26,13 @@ function App() {
       <Navbar />
       <Routes>
         <Route element={<ProtectedRoute />}>
-          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
         <Route path='/' element={<Home />}></Route>
         <Route path="/Login" element={<Login />} />
+        <Route path="/Logout" element={<Logout />} />
         <Route path="/Singup" element={<Singup />} />
+        <Route path='*' element={<NotFound/>} />
       </Routes>
     </>
   )
