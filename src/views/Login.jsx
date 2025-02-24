adbede6c8e55152ff50095cd42d177e9681ee9fc
Ali AB <aliabdev07@gmail.com>
2025-02-24 23:04:22 +0000

    Update Login.jsx
    - change Sing up to Register

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -98,7 +98,7 @@ const handleSubmit = async (e) => {
 
           <p className="mt-10 text-center text-sm/6 text-gray-500">
             Not a member?{' '}
-            <Link to='/Singup' className="font-semibold text-indigo-600 hover:text-indigo-500">
+            <Link to='/Register' className="font-semibold text-indigo-600 hover:text-indigo-500">
               Sing up
             </Link>
           </p>
