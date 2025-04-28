08df658a395697d8ea72c399ae98d737e4209a66
Ali AB <aliabdev07@gmail.com>
2025-04-29 00:07:17 +0100

    Update Navbar.jsx
    - Add profile link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -108,15 +108,7 @@ export const Navbar = () => {
                     href="#"
                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                   >
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Earnings
+                    Profile
                   </a>
                 </li>
                 <li>
